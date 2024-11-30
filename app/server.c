01724c560e4e2889841f25e3c7fde0fe21dbae07
Talip Tun <taliptun62@gmail.com>
2024-11-30 15:17:08 +0000

    [any message]

--- app/server.c
+++ app/server.c
@@ -56,7 +56,7 @@ int main() {
 	printf("Waiting for a client to connect...\n");
 	client_addr_len = sizeof(client_addr);
 	
-	client_fd = (server_fd, (struct sockaddr *) &client_addr, &client_addr_len);
+	client_fd = accept(server_fd, (struct sockaddr *) &client_addr, &client_addr_len);
 
 	if (client_fd < 0) {
         printf("Accept failed: %s\n", strerror(errno));
