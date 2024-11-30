8ca14abb974579328f090d60d6510706288b1102
Talip Tun <taliptun62@gmail.com>
2024-11-30 19:36:52 +0000

    respond with 200 and 404 after reading the http url

--- app/server.c
+++ app/server.c
@@ -88,10 +88,12 @@ int main() {
 	char request[] = "";
 	int response = recv(client_fd, request, sizeof(request), 0);
 	if (strstr("404" , request) == NULL || response < 0) {
+		printf("%s\n", strstr(request, "404") ? "Substring found" : "Substring not found");
 		printf("2\n");
 		printf("%s\n",request);
 		send(client_fd, response_404, strlen(response_404), 0);
     } else { 
+		printf("%s\n", strstr(request, "404") ? "Substring found" : "Substring not found");
 		printf("3\n");
 		printf("%s\n",request);
 		send(client_fd, response_200, strlen(response_200), 0);
